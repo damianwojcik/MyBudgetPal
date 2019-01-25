c539a8cbe23cf60aed3c914872d0dd8939463806
Damian Wójcik <damian.wojcik@agora.pl>
2019-01-25 17:24:32 +0100

    remove state from App, create Dashboard container /w state

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,5 +1,3 @@
 # Todo:
-- remove state from app
-- state should be in page compontent (stateful)
 - add 'add' route
 - add prop-types
--- src/App.js
+++ src/App.js
@@ -1,70 +1,12 @@
 import React, { Component } from 'react';
-
-import Header from './Components/Header';
-import Status from './Components/Status';
-import BottomBar from './Components/BottomBar';
-import Diary from './Components/Diary';
-
+import Dashboard from './containers/Dashboard';
 import classes from './App.module.css';
 
 class App extends Component {
-  state = {
-    month: 'January',
-    data: [
-      {
-          id: 1,
-          created: "2019-01-05T04:33:35Z",
-          type: 'grocery',
-          title: 'biedronka',
-          price: -38.15
-      },
-      {
-          id: 2,
-          created: "2019-01-05T04:33:35Z",
-          type: 'salary',
-          title: 'wypłata',
-          price: 4400
-      },
-      {
-          id: 3,
-          created: "2019-01-05T04:33:35Z",
-          type: 'house',
-          title: 'Ikea biurko',
-          price: -200
-      },
-      {
-          id: 4,
-          created: "2019-01-06T04:33:35Z",
-          type: 'grocery',
-          title: 'lidl zakupy',
-          price: -51.23
-      },
-      {
-          id: 5,
-          created: "2019-01-02T04:33:35Z",
-          type: 'grocery',
-          title: 'piekania',
-          price: -6.21
-      }
-    ],
-  }
-  deleteEntryHandler(index) {
-    const entryIndex = this.state.data.findIndex(entry => {
-      return entry.id === index;
-    });
-    const data = [...this.state.data];
-    data.splice(entryIndex, 1);
-    this.setState({ data: data });
-  }
   render() {
     return (
       <div className={classes.App}>
-        <Header month={this.state.month} />
-        <Status data={this.state.data} />
-        <Diary
-          data={this.state.data}
-          click={this.deleteEntryHandler.bind(this)} />
-        <BottomBar />
+        <Dashboard />
       </div>
     );
   }
